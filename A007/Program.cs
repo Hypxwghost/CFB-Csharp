23e639b8fa4def150dc5ed16e74a2e1eef2d369c
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-18 18:51:38 -0300

    a007

---
+++ A007/Program.cs
@@ -0,0 +1,10 @@
+﻿class A007
+{
+    static void Main()
+    {
+        const string canal = "CFB Cursos";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal {0} \nPi: {1}", canal, pi);
+    }
+}
