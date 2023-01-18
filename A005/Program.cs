c8a037c4d2725ec607213de1c375c0bfdf0f5012
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-18 14:47:07 -0300

    a005

---
+++ A005/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class A005
+{
+    static void Main()
+    {
+        //  & = AND / E
+        //  | = OR / OU
+
+        bool res = (5>3) & (10>5); 
+        Console.WriteLine(res);
+    }
+}
