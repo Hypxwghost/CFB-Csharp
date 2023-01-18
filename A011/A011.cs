b0264f6b079e6815f5bce215baf9fc4b1555785a
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-18 20:00:36 -0300

    a011

---
+++ A011/A011.cs
@@ -0,0 +1,15 @@
+﻿class A011
+{
+    static void Main()
+    {
+        //float n1 = 10.5f;
+        //int n2 = (int)n1;
+
+        //Console.WriteLine(n2);
+
+        int vInt = 10;
+        short vShort = (short)vInt;
+
+        Console.WriteLine(vShort);
+    }
+}
