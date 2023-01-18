98e7a2af39470e5d2b4c070f459ac801d9104b84
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-18 19:20:28 -0300

    a009

---
+++ A008/A008.cs
@@ -0,0 +1,17 @@
+﻿class A008
+{
+    static void Main()
+    {
+        int v1, v2, soma;
+
+        Console.Write("Digite o primeiro valor: ");
+        v1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo valor: ");
+        v2 = Convert.ToInt32(Console.ReadLine());
+
+        soma = v1 + v2;
+
+        Console.WriteLine("A soma de {0} mais {1} é igual a {2}", v1, v2, soma);
+    }
+}
