d99a60cd7f447fe8ea36b8dc72ca4439d9171383
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-18 19:46:45 -0300

    A010

---
+++ A010/Program.cs
@@ -0,0 +1,17 @@
+﻿class A010
+{
+    enum DiasSemana { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };
+
+    static void Main()
+    {
+        //Random rnd = new Random();
+        //int aleatorio = rnd.Next(6);
+        //DiasSemana ds = (DiasSemana)aleatorio;
+
+        int ds = (int)DiasSemana.Sexta;
+        DiasSemana dia = (DiasSemana)ds;
+
+        Console.WriteLine(ds);
+        Console.WriteLine(dia);
+    }
+}
