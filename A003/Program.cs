32b10b50e32c1d0f72c9229abb64a64dd6b97914
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-18 14:46:56 -0300

    a003

---
+++ A003/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class A003
+{
+    static void Main()
+    {
+        int num1, num2;
+
+        num1 = 10;
+        num2 = 2;
+
+        Console.WriteLine("A multiplicacao de " + num1 + " e " + num2 + " é igual a " + num1 * num2);
+    }
+}
