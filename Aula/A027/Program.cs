06857edef6a119d03f1e8a41ba9db02596ab3be0
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-24 17:39:49 -0300

    A28

--- Aula/A027/Program.cs
+++ Aula/A027/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class A027
+{
+    static void Main()
+    {
+        Soma();
+    }
+
+    static void Soma(params int[] n)
+    {
+        int res = 0;
+
+        if (n.Length < 1)
+        {
+            Console.WriteLine("Não existem valores a serem somados!");
+        }
+        else if (n.Length < 2)
+        {
+            Console.WriteLine("Valores insuficientes para soma: {0}", n[0]);
+        }
+        else
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                res += n[i];
+
+            }
+        }
+        Console.WriteLine("A soma dos valores é: {0}", res);
+    }
+}
