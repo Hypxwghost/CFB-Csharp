47663c7e43a4f83b02c308ff6c53de7d78b8cdf0
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-24 16:21:42 -0300

    A20

--- Aula/A020/Program.cs
+++ Aula/A020/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class A020
+{
+    static void Main()
+    {
+        int[] num = new int[10];
+
+        int i = 0;
+
+        while (i < num.Length)
+        {
+            num[i] = i;
+            Console.WriteLine(num[i]);
+            i++;
+        }
+
+        i -= 1;
+
+        while (i > 0)
+        {
+            num[i] = i;
+            Console.WriteLine(num[i]);
+            i--;
+        }
+
+        Console.WriteLine("Fim");
+    }
+}
