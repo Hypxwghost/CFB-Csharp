c43f62719e13d93675b69b4f8cdfa1808381bcbc
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-24 16:21:53 -0300

    A22

--- Aula/A022/Program.cs
+++ Aula/A022/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class A022
+{
+    static void Main()
+    {
+        int[] num = new int[5] { 11, 22, 33, 44, 55 };
+
+        /*for (int i = 0; i < num.Length; i++)
+        {
+            Console.WriteLine(num[i]);
+        }*/
+
+        foreach (int n in num)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
