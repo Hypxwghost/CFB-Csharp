f762a43b39deb177550b29bad0a0573cc4d03944
Khalli <60857997+Hypxwghost@users.noreply.github.com>
2023-01-24 17:39:43 -0300

    A26

--- Aula/A026/Program.cs
+++ Aula/A026/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class A025
+{
+    static void Main()
+    {
+        int divid, divis, quoc, rest;
+        divid = 10; divis = 3; quoc = Divide(divid, divis, out rest);
+
+        Console.WriteLine("{0}/{1}:quociente={2} e resto={3}", divid, divis, quoc, rest);
+    }
+
+    static int Divide(int dividendo, int divisor, out int resto)
+    {
+        int quociente;
+        quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+        return quociente;
+    }
+
+}
